4a7eb31cc4aa008a18810edf94174c6bcb52ca1d
Pedro Andrade <pedrogamerp@gmail.com>
2024-11-21 09:21:21 -0300

    fix: correct MEDIA_ROOT setting in production configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "static"]
 else:
     STATIC_ROOT = BASE_DIR / "static"
-    MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
